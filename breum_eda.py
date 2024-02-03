ec3d6b3837113f7e5aa0aac9206775b177831339
Kayla Breum <156029314+kcbreum@users.noreply.github.com>
2024-02-03 11:12:13 -0600

    Update breum_eda.py

--- breum_eda.py
+++ breum_eda.py
@@ -1,3 +1,8 @@
+''' Project 4 - Jupyter Notebook
+## By: Kayla Breum, guided by Denise Case
+### The purpose of this project is to use a combination of Python and Markdown to demonstrate the skills needed to create an initial data story in Jupyter Notebook through guided exploratory data analysis.
+'''
+
 # Import Dependencies
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -5,8 +10,11 @@ import seaborn as sns
 
 
 # Data Acquisition
+
+## Load the Iris dataset into DataFrame
 df = sns.load_dataset('iris')
 
+## Inspect first rows of the DataFrame
 print(df.head())
 
 
@@ -20,25 +28,44 @@ print(df.dtypes)
 print(df.describe())
 
 
-# Initial Data Distribution
-# Inspect histogram by numerical column
+# Initial Data Distribution for Numerical Columns
+
+## Inspect histogram by numerical column
 df['sepal_length'].hist()
 
-# Inspect histograms for all numerical columns
+## Inspect histograms for all numerical columns
 df.hist()
 
-# Show all plots
+## Show all plots
 plt.show()
 
-# Inspect value counts by categorical column
+
+# Initial Data Distribution for Categorical Columns
+
+## Inspect value counts by categorical column
 df['species'].value_counts()
 
-# Inspect value counts for all categorical columns
+## Inspect value counts for all categorical columns
 for col in df.select_dtypes(include=['object', 'category']).columns:
     # Display count plot
     sns.countplot(x=col, data=df)
     plt.title(f'Distribution of {col}')
     plt.show()
 
-# Show all plots
+## Show all plots
+plt.show()
+
+
+# Initial data transformation and feature engineering
+## Renaming columns
+df.rename(columns={'sepal_length': 'Sepal Length'}, inplace=True)
+df.rename(columns={'sepal_width': 'Sepal Width'}, inplace=True)
+df.rename(columns={'petal_length': 'Petal Length'}, inplace=True)
+df.rename(columns={'petal_width': 'Petal Width'}, inplace=True)
+
+## Adding a new column
+df['Sepal Area'] = df['Sepal Length'] * df['Sepal Width']
+
+# Initial Visualizations
+sns.pairplot(df, hue='species')
 plt.show()
